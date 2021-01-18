febafe9695fad74be05779014ac6626504be2cf1
github chetali-3 <vchetalibhatt@gmail.com>
2021-01-18 12:34:44 +0000

    add hello.py

--- session-1/hello.py
+++ session-1/hello.py
@@ -1,1 +1,13 @@
-1 print("Hello world")
+# about %
+
+#assigning some value to variable x
+ x = 9
+
+#assigning some value to variable y
+ y = 3
+ 
+ z = x % y
+
+#printing variable z to console
+ 
+ print(z)
