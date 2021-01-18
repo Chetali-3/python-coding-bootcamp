be8d1b85ef0abcaaaa2ed40024fd55be131fa049
github chetali-3 <vchetalibhatt@gmail.com>
2021-01-18 18:06:36 +0000

    add hello.py

---
+++ session-3/hello.py
@@ -0,0 +1,41 @@
+ """ 
+# if statement
+
+num = 6
+
+if  ( num == 5 ) :
+      print("The num is equal to 5")
+
+ #The condition is false
+if  ( num > 5 ) :
+     print("The num is greater than 5")
+ 
+ """
+
+# if-else statement 
+
+num = 30
+if  ( num <= 50 ) :
+      print("The num is less than or equal to 50")
+
+else :
+      print("The num is greater than 50")
+ 
+ """
+
+#if-elif-else statement
+light = "Orange"
+
+if  light == "Green" :
+      print("Go")
+
+elif  light == "Yellow" :
+          print("Caution")
+
+elif  light == "Red" :
+          print("Stop")
+
+else :
+          print("Incorrect light signal")
+
+ """
