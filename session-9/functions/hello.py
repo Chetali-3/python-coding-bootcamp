9671c43aff6bf1db93c3740bfb3ae17e3efea080
github chetali-3 <vchetalibhatt@gmail.com>
2021-01-19 19:43:07 +0000

    add types of functions

---
+++ session-9/functions/hello.py
@@ -0,0 +1,24 @@
+#Types of functions
+
+"""
+# finding out length
+name = input("Enter your name")
+length = len(name)
+print(length)
+"""
+"""
+# capitalising
+name = input("Enter your name")
+new_name = name.capitalize()
+print(new_name)
+"""
+"""
+#finding out a letter
+name = input("Enter your name")
+new_name = name.find(input("enter the word you wanna find"))
+print(new_name)
+"""
+#replacing
+name = input("Enter your name")
+new_name = name.replace("li", "na")
+print (new_name)
