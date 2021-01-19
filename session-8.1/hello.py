aab9ccecb88eac56999d9acb0b7ae04e03fa6e79
github chetali-3 <vchetalibhatt@gmail.com>
2021-01-19 17:24:53 +0000

    add types of loops

---
+++ session-8.1/hello.py
@@ -0,0 +1,26 @@
+# Loop and Iterations
+
+"""
+#Continue
+
+for i in range(1, 6) :
+        if i == 4 :
+            continue
+        else :
+             print(i)
+"""
+"""
+#Break
+for i in range(1, 6) :
+        if i == 4 :
+            break
+        else :
+           print(i)
+"""
+#While
+
+counter = 4
+while (counter <5) :
+           print(counter)
+
+counter = counter + 1
