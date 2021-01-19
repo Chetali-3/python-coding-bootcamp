9bf79b9724c647808a4dd7dd8d5c443160e1cd38
github chetali-3 <vchetalibhatt@gmail.com>
2021-01-19 17:00:01 +0000

    add loop types

---
+++ session-4/hello.py
@@ -0,0 +1,28 @@
+# Loop
+
+# for loop
+# A sequence from 1to 10
+
+for i in range(1, 11) :
+       if i % 2 == 0 :
+            print(i, "is even")
+       else :
+            print(i, "is odd")
+"""
+"""
+for i in range(1, 11 ,3) :
+       if i % 2 == 0 :
+            print(i, "is even")
+       else :
+            print(i, "is odd")
+"""
+# While loop
+
+LampTurnedOn = "true"
+
+while  LampTurnedOn == "true" :
+        LampTurnedOn = input("Please continue reading your book, Is the light turned on?  (Say true if on) : ") 
+        
+        
+        
+print("Lamp turned off! Go for sleep")
